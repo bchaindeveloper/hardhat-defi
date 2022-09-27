ed80257843e505a3bc3eed4d0d76ded27375650e
bchaindeveloper <bcdev3@protonmail.com>
2022-09-27 16:49:45 -0400

    MainNet Fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 require("solidity-coverage");
 require("hardhat-deploy");
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || "";
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 const GOERLI_RPC_URL =
   process.env.GOERLI_RPC_URL ||
-  "https://eth-goerli.g.alchemy.com/v2/apItZ5us4xzjX9Wsc7jcJwv77sSNnrT0";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+  "https://eth-goerli.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
+  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
@@ -16,14 +26,18 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 31337,
-      // gasPrice: 130000000000,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+    },
+    localhost: {
+      chainId: 31337,
     },
     goerli: {
       url: GOERLI_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 5,
       blockConfirmations: 6,
-      gas: 6000000,
     },
   },
   solidity: {
@@ -32,7 +46,7 @@ module.exports = {
         version: "0.8.8",
       },
       {
-        version: "0.6.6",
+        version: "0.6.12",
       },
       {
         version: "0.4.19",
@@ -48,23 +62,11 @@ module.exports = {
     outputFile: "gas-report.txt",
     noColors: true,
     // coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "MATIC",
   },
   namedAccounts: {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
       1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-    },
-  },
-  mocha: {
-    timeout: 500000,
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-    user: {
-      default: 1,
     },
   },
 };
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,10 +1,13 @@
-async function main() {
+const { getWeth, AMOUNT } = require("../scripts/getWeth.js");
+const { networkConfig } = require("../helper-hardhat-config");
 
+async function main() {
+  await getWeth();
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
