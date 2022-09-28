d9f6516f7988fe413c5d28176deb6da5b3567458
bchaindeveloper <bcdev3@protonmail.com>
2022-09-28 09:26:07 -0400

    DAI Price Feed

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -7,12 +7,47 @@ async function main() {
   const { deployer } = await getNamedAccounts();
   const lendingPool = await getLendingPool(deployer);
   console.log(`LendingPool address ${lendingPool.address}`);
-
+  // deposit
   const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  // approve
   await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer);
   console.log("Depositing...");
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
   console.log("Deposited!");
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+    lendingPool,
+    deployer
+  );
+  const daiPrice = await getDaiPrice();
+  const amountDaiToBorrow =
+    availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber());
+  const amountDaiToBorrowWei = ethers.utils.parseEther(
+    amountDaiToBorrow.toString()
+  );
+  console.log(`You can borrow ${amountDaiToBorrow.toString()} DAI`);
+  // borrow
+}
+
+
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    networkConfig[network.config.chainId].daiEthPriceFeed
+  );
+  const price = (await daiEthPriceFeed.latestRoundData())[1];
+  console.log(`The DAI/ETH price is ${price.toString()}`);
+  return price;
+  // 0x773616e4d11a78f511299002da57a0a94577f1f4;
+}
+
+async function getBorrowUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account);
+  console.log(`You have ${totalCollateralETH} worth of ETH deposited.`);
+  console.log(`You have ${totalDebtETH} worth of ETH borrowed.`);
+  console.log(`You can borrow ${availableBorrowsETH} worth of ETH.`);
+  return { availableBorrowsETH, totalDebtETH };
 }
 
 async function getLendingPool(account) {
