ee1e931d89319716e7ed86495ee7d43f610f0e76
bchaindeveloper <bcdev3@protonmail.com>
2022-09-28 08:45:27 -0400

    IERC20.sol

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -7,12 +7,18 @@ async function main() {
   const { deployer } = await getNamedAccounts();
   const lendingPool = await getLendingPool(deployer);
   console.log(`LendingPool address ${lendingPool.address}`);
+
+  const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer);
+  console.log("Depositing...");
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
+  console.log("Deposited!");
 }
 
 async function getLendingPool(account) {
   const lendingPoolAddressesProvider = await ethers.getContractAt(
     "ILendingPoolAddressesProvider",
-    "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+    networkConfig[network.config.chainId].lendingPoolAddressesProvider,
     account
   );
   const lendingPoolAddress =
@@ -22,8 +28,24 @@ async function getLendingPool(account) {
     lendingPoolAddress,
     account
   );
+  return lendingPool;
 }
-return lendingPool;
+
+async function approveErc20(
+  erc20Address,
+  spendAddress,
+  amountToSpend,
+  account
+) {
+  const erc20Token = await ethers.getContractAt(
+    "IERC20",
+    erc20Address,
+    account
+  );
+  const tx = await erc20Token.approve(spendAddress, amountToSpend);
+  await tx.wait(1);
+  console.log("Approved");
+}
 
 main()
   .then(() => process.exit(0))
